2de7b8693f7ef70ddbfc0fbb6060ebb1981af0e8
mreiter <matthewreiter@hotmail.com>
2020-05-10 02:08:47 -0400

    Create a CLI to convert a blueprint to JSON.

---
+++ Blueprint Reader/BlueprintReader.cs
@@ -0,0 +1,20 @@
+﻿using BlueprintCommon;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace BlueprintReader
+{
+    public static class BlueprintReader
+    {
+        public static void Run(IConfigurationRoot configuration)
+        {
+            var inputBlueprintFile = configuration["InputBlueprint"];
+            var outputJsonFile = configuration["OutputJson"];
+
+            var json = BlueprintUtil.ReadBlueprintFileAsJson(inputBlueprintFile);
+            var jsonObj = JsonSerializer.Deserialize<object>(json);
+
+            BlueprintUtil.WriteOutJson(outputJsonFile, jsonObj);
+        }
+    }
+}
---
+++ Blueprint Reader/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BlueprintReader
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            BlueprintReader.Run(configuration);
+        }
+    }
+}
