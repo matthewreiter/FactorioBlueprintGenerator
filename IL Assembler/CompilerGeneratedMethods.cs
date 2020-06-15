91d7124c3392c621940f572966ad1881bca3bcc6
mreiter <matthewreiter@hotmail.com>
2020-06-15 03:57:25 -0400

    Move ProgramBuilder and compiler generated methods into their own files and implement writing to arbitrary addresses.

---
+++ IL Assembler/CompilerGeneratedMethods.cs
@@ -0,0 +1,38 @@
+﻿using CompilerCommon;
+using SeeSharp.Runtime;
+using System.Reflection;
+
+namespace Assembler
+{
+    public partial class ProgramBuilder
+    {
+        private void AddCompilerGeneratedMethod(MethodBase method)
+        {
+            if (method.DeclaringType == typeof(Memory))
+            {
+                if (method.Name == nameof(Memory.Read))
+                {
+                    AddInstruction(Instruction.Pop(4)); // Address
+                    AddInstructions(Instruction.NoOp(4));
+                    AddInstruction(Instruction.ReadMemory(outputRegister: ReturnRegister, addressRegister: 4));
+                    AddInstructions(Instruction.NoOp(4));
+                }
+                else if (method.Name == nameof(Memory.Write))
+                {
+                    AddInstruction(Instruction.Pop(5)); // Value
+                    AddInstruction(Instruction.Pop(4)); // Address
+                    AddInstructions(Instruction.NoOp(4));
+                    AddInstruction(Instruction.WriteMemory(addressRegister: 4, inputRegister: 5));
+                }
+                else if (method.Name == nameof(Memory.ReadSignal))
+                {
+                    AddInstruction(Instruction.Pop(5)); // Signal
+                    AddInstruction(Instruction.Pop(4)); // Address
+                    AddInstructions(Instruction.NoOp(4));
+                    AddInstruction(Instruction.ReadSignal(outputRegister: ReturnRegister, addressRegister: 4, signalRegister: 5));
+                    AddInstructions(Instruction.NoOp(4));
+                }
+            }
+        }
+    }
+}
--- See Sharp Runtime/Memory.cs
+++ See Sharp Runtime/Memory.cs
@@ -5,9 +5,18 @@ namespace SeeSharp.Runtime
     public static class Memory
     {
         [CompilerGenerated]
+        public static int Read(int address)
+        {
+            return 0;
+        }
+
+        [CompilerGenerated]
         public static int ReadSignal(int address, Signal signal)
         {
             return 0;
         }
+
+        [CompilerGenerated]
+        public static void Write(int address, int value) { }
     }
 }
