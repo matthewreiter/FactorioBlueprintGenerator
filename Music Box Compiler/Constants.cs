b1d4316c6fd9da41b0467cc69cd42fe12453e131
mreiter <matthewreiter@hotmail.com>
2020-06-28 22:57:06 -0400

    Add missing drum names.

--- Music Box Compiler/Constants.cs
+++ Music Box Compiler/Constants.cs
@@ -4,6 +4,25 @@ namespace MusicBoxCompiler
 {
     public static class Constants
     {
-        public static readonly List<string> Drums = new List<string> { "Kick 1", "Kick 2", "Snare 1", "Snare 2", "Snare 3", "High Hat 1", "High Hat 2" };
+        public static readonly List<string> Drums = new List<string>
+        {
+            "Kick 1",
+            "Kick 2",
+            "Snare 1",
+            "Snare 2",
+            "Snare 3",
+            "High Hat 1",
+            "High Hat 2",
+            "Fx",
+            "High Q",
+            "Percussion 1",
+            "Percussion 2",
+            "Crash",
+            "Reverse Cymbal",
+            "Clap",
+            "Shaker",
+            "Cowbell",
+            "Triangle"
+        };
     }
 }
