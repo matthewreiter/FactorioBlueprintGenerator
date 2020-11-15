f909811617aebcaf61225ad09028e19c8c6a9ed7
mreiter <matthewreiter@hotmail.com>
2020-11-15 03:48:56 -0500

    Adjust the substation height.

--- Memory Initializer/RomGenerator.cs
+++ Memory Initializer/RomGenerator.cs
@@ -154,7 +154,7 @@ namespace MemoryInitializer
             }
 
             var substationWidth = (width + 7) / 16 + 1;
-            var substationHeight = (gridHeight + 3) / 18 + 1;
+            var substationHeight = (gridHeight + 2) / 18 + 1;
 
             entities.AddRange(CreateSubstations(substationWidth, substationHeight, xOffset, gridHeight % 18 - 6 + yOffset, width * height * entitiesPerCell + 1));
 
