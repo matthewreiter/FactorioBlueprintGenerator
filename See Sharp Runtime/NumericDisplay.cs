cd24f33fbcd4908d8a471af45a5e23dc7347e37e
mreiter <matthewreiter@hotmail.com>
2020-12-03 01:17:25 -0500

    Add a numeric display to the computer.

---
+++ See Sharp Runtime/NumericDisplay.cs
@@ -0,0 +1,17 @@
+﻿using SeeSharp.Runtime.CompilerServices;
+
+namespace SeeSharp.Runtime
+{
+    public class NumericDisplay
+    {
+        private const int BaseAddress = 35841;
+
+        public static int Value
+        {
+            [Inline]
+            get { return Memory.Read(BaseAddress); }
+            [Inline]
+            set { Memory.Write(BaseAddress, value); }
+        }
+    }
+}
