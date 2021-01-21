8365c7e61f5f98797b9a34488fdc198da7ec63b5
mreiter <matthewreiter@hotmail.com>
2021-01-20 23:45:11 -0500

    Convert the music manager to a top-level program.

--- Music Manager/Program.cs
+++ Music Manager/Program.cs
@@ -1,34 +1,28 @@
 ﻿using Music;
 using SeeSharp.Runtime;
+using System.Threading;
 
-namespace MusicManager
+var previousPosition = 0;
+
+while (true)
 {
-    public class Program
+    var charge = FactoryNetwork.GetValue(Signal.A);
+    var isLow = MusicBox.GetPlayingSongMetadata(MusicBox.SongMetadataField.MetadataAddress) == SongMetadataAddresses.Cynthia;
+
+    if (charge < 50 && !isLow)
     {
-        public static void Main()
-        {
-            var previousPosition = 0;
+        MusicBox.SelectSongMetadata(SongMetadataAddresses.Cynthia); // Do this each time in case songs are redeployed while the program is running
+        var cynthia = MusicBox.GetSelectedSongMetadata(MusicBox.SongMetadataField.SongAddress);
 
-            while (true)
-            {
-                var charge = FactoryNetwork.GetValue(Signal.A);
-                var isLow = MusicBox.GetPlayingSongMetadata(MusicBox.SongMetadataField.MetadataAddress) == SongMetadataAddresses.Cynthia;
-
-                if (charge < 50 && !isLow)
-                {
-                    MusicBox.SelectSongMetadata(SongMetadataAddresses.Cynthia); // Do this each time in case songs are redeployed while the program is running
-                    var cynthia = MusicBox.GetSelectedSongMetadata(MusicBox.SongMetadataField.SongAddress);
-
-                    previousPosition = MusicBox.Position;
-                    MusicBox.Position = cynthia;
-                }
-                else if (charge > 55 && isLow)
-                {
-                    MusicBox.Position = previousPosition;
-                }
-
-                NumericDisplay.Value = MusicBox.GetPlayingSongMetadata(MusicBox.SongMetadataField.TrackNumber);
-            }
-        }
+        previousPosition = MusicBox.Position;
+        MusicBox.Position = cynthia;
+        Thread.Sleep(10000);
     }
+    else if (charge > 55 && isLow)
+    {
+        MusicBox.Position = previousPosition;
+        Thread.Sleep(10000);
+    }
+
+    NumericDisplay.Value = MusicBox.GetPlayingSongMetadata(MusicBox.SongMetadataField.TrackNumber);
 }
