686d2c6c686504d6ff72d8ff644d9f43622f2de0
mreiter <matthewreiter@hotmail.com>
2021-01-30 01:43:34 -0500

    Add the ability to clear the screen.

---
+++ See Sharp Runtime/Screen.cs
@@ -0,0 +1,23 @@
+﻿using SeeSharp.Runtime.CompilerServices;
+
+namespace SeeSharp.Runtime
+{
+    public static class Screen
+    {
+        private const int BaseAddress = 37889;
+        private const int ClearScreenAddress = BaseAddress;
+
+        [Inline]
+        public static void ClearScreen(PixelValue value = PixelValue.Clear)
+        {
+            Memory.Write(ClearScreenAddress, (int)value);
+        }
+
+        public enum PixelValue
+        {
+            Clear = -1,
+            Ignore = 0,
+            Set = 1
+        }
+    }
+}
