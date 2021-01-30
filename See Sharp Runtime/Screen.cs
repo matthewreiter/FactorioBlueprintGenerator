4ef98de7630d1a6145586bc8f7cfd4b5d3b8f10a
mreiter <matthewreiter@hotmail.com>
2021-01-30 02:14:00 -0500

    Add the ability to set the screen color.

--- See Sharp Runtime/Screen.cs
+++ See Sharp Runtime/Screen.cs
@@ -6,6 +6,7 @@ namespace SeeSharp.Runtime
     {
         private const int BaseAddress = 37889;
         private const int ClearScreenAddress = BaseAddress;
+        private const int ScreenColorAddress = BaseAddress + 1;
 
         [Inline]
         public static void ClearScreen(PixelValue value = PixelValue.Clear)
@@ -13,11 +14,28 @@ namespace SeeSharp.Runtime
             Memory.Write(ClearScreenAddress, (int)value);
         }
 
+        [Inline]
+        public static void SetScreenColor(Color color)
+        {
+            Memory.Write(ScreenColorAddress, (int)color);
+        }
+
         public enum PixelValue
         {
             Clear = -1,
             Ignore = 0,
             Set = 1
+        }
+
+        public enum Color
+        {
+            None,
+            Red,
+            Blue,
+            Yellow,
+            Pink,
+            Cyan,
+            White
         }
     }
 }
