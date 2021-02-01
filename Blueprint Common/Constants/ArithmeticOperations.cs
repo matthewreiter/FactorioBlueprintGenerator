523c854f6d0ed14179cd8599ca9cddafe689fb0a
mreiter <matthewreiter@hotmail.com>
2021-02-01 03:10:58 -0500

    Add remaining arithmetic operations.

--- Blueprint Common/Constants/ArithmeticOperations.cs
+++ Blueprint Common/Constants/ArithmeticOperations.cs
@@ -3,5 +3,15 @@
     public static class ArithmeticOperations
     {
         public const string Multiplication = "*";
+        public const string Division = "/";
+        public const string Addition = "+";
+        public const string Subtraction = "-";
+        public const string Modulus = "%";
+        public const string Exponentiation = "^";
+        public const string LeftShift = "<<";
+        public const string RightShift = ">>";
+        public const string And = "AND";
+        public const string Or = "OR";
+        public const string Xor = "XOR";
     }
 }
