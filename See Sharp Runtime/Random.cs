3e975d093fbd41632791b93a3353f0d6655820fb
mreiter <matthewreiter@hotmail.com>
2021-02-12 02:57:18 -0500

    Add a random number generator.

---
+++ See Sharp Runtime/Random.cs
@@ -0,0 +1,22 @@
+﻿namespace SeeSharp.Runtime
+{
+    public class Random
+    {
+        private const int A = 1103515245;
+        private const int C = 12345;
+
+        private int Seed = Clock.AbsoluteTicks;
+
+        public int Next()
+        {
+            // Generates a random number using the linear congruential method
+            Seed = A * Seed + C;
+            return Seed & 0x3FFFFFFF;
+        }
+
+        public int Next(int maxValue)
+        {
+            return Next() % maxValue;
+        }
+    }
+}
