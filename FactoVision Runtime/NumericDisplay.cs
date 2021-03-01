73116526f6a12c8f45302936ab421600589b05b7
mreiter <matthewreiter@hotmail.com>
2021-02-28 21:18:33 -0500

    Add a second numeric display.

--- FactoVision Runtime/NumericDisplay.cs
+++ FactoVision Runtime/NumericDisplay.cs
@@ -6,12 +6,23 @@ namespace FactoVision.Runtime
     {
         private const int BaseAddress = 35841;
 
+        private const int Display1Address = BaseAddress;
+        private const int Display2Address = BaseAddress + 1;
+
         public static int Value
         {
             [Inline]
-            get { return Memory.Read(BaseAddress); }
+            get { return Memory.Read(Display1Address); }
             [Inline]
-            set { Memory.Write(BaseAddress, value); }
+            set { Memory.Write(Display1Address, value); }
+        }
+
+        public static int Value2
+        {
+            [Inline]
+            get { return Memory.Read(Display2Address); }
+            [Inline]
+            set { Memory.Write(Display2Address, value); }
         }
     }
 }
