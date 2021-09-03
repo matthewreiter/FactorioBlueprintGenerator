3c4e31386ef6ae6d2c4a2e4be9d5ef0c8517a525
mreiter <matthewreiter@hotmail.com>
2021-09-03 17:30:31 -0400

    Add test graphics to the music manager.

--- Music Manager/Program.cs
+++ Music Manager/Program.cs
@@ -2,7 +2,17 @@
 using FactoVision.Runtime;
 using System.Threading;
 
+InitializeScreen();
+LoadSprites();
+
 var previousPosition = 0;
+var screenColor = 0;
+var rnd = new Random();
+//var text = new char[] { 'W', 'h', 'a', 't', ' ', 'i', 's', ' ', 'g', 'o', 'i', 'n', 'g', ' ', 'o', 'n', ' ', 'h', 'e', 'r', 'e', '?', ' ', '(', 'I', ' ', 'm', 'e', 'a', 'n', ',', ' ', 'c', 'o', 'm', 'e', ' ', 'o', 'n', '!', ')' };
+var text = new char[] { 'H', 'e', 'l', 'l', 'o', '!' };
+
+Screen.CharacterX = rnd.Next(50);
+Screen.CharacterY = rnd.Next(50);
 
 while (true)
 {
@@ -21,6 +31,8 @@ while (true)
         }
         else
         {
+            MusicBox.Position = GetSongAddress(SongMetadataAddresses.Blank);
+            Thread.Sleep(2000);
             MusicBox.Position = songAddress;
         }
     }
@@ -38,11 +50,140 @@ while (true)
         Thread.Sleep(10000);
     }
 
+    Screen.ClearScreen();
+    Screen.SetScreenColor((Screen.Color)screenColor);
+    Screen.ClearScreen(Screen.PixelValue.Set);
     NumericDisplay.Value = MusicBox.GetPlayingSongMetadata(MusicBox.SongMetadataField.TrackNumber);
+    Screen.ClearScreen();
+
+    Screen.SpriteX = rnd.Next(100);
+    Screen.SpriteY = rnd.Next(100);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+    Screen.DrawSprite(2);
+    Screen.DrawSprite(3);
+    Screen.DrawSprite(4);
+    Screen.DrawSprite(1);
+
+    Screen.Print('T');
+    Screen.Print('r');
+    Screen.Print('a');
+    Screen.Print('c');
+    Screen.Print('k');
+    Screen.Print(' ');
+    Screen.Print('#');
+    Screen.Print(NumericDisplay.Value);
+
+    //Screen.Print("What is going on here?");
+    //Screen.Print(text);
+
+    screenColor = (screenColor + 1) % 8;
 }
 
 static int GetSongAddress(int metadataAddress)
 {
     MusicBox.SelectSongMetadata(metadataAddress);
     return MusicBox.GetSelectedSongMetadata(MusicBox.SongMetadataField.SongAddress);
+}
+
+static void InitializeScreen()
+{
+    Screen.ClearScreen();
+    Screen.SetScreenColor(Screen.Color.White);
+    Screen.Font = 1;
+    Screen.AutoAdvanceCharacter = true;
+    Screen.CharacterX = 70;
+    Screen.CharacterY = 56;
+    Screen.Print('L');
+    Screen.Print('o');
+    Screen.Print('a');
+    Screen.Print('d');
+    Screen.Print('i');
+    Screen.Print('n');
+    Screen.Print('g');
+    Screen.Print('.');
+    Screen.Print('.');
+    Screen.Print('.');
+}
+
+static void LoadSprites()
+{
+    Screen.WriteSprite(1, 9, 9, new int[]
+    {
+        0b001111100,
+        0b010000010,
+        0b100101001,
+        0b100101001,
+        0b100000001,
+        0b101000101,
+        0b100111001,
+        0b010000010,
+        0b001111100
+    });
+
+    Screen.WriteSprite(2, 9, 9, new int[]
+    {
+        0b001111100,
+        0b010000010,
+        0b100001001,
+        0b101100101,
+        0b100000101,
+        0b101100101,
+        0b100001001,
+        0b010000010,
+        0b001111100
+    });
+
+    Screen.WriteSprite(3, 9, 9, new int[]
+    {
+        0b001111100,
+        0b010000010,
+        0b100111001,
+        0b101000101,
+        0b100000001,
+        0b100101001,
+        0b100101001,
+        0b010000010,
+        0b001111100
+    });
+
+    Screen.WriteSprite(4, 9, 9, new int[]
+    {
+        0b001111100,
+        0b010000010,
+        0b100100001,
+        0b101001101,
+        0b101000001,
+        0b101001101,
+        0b100100001,
+        0b010000010,
+        0b001111100
+    });
 }
