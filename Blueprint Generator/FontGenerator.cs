aa9b1d12e7ee96df58b6916c4ffc9abbaee11a81
Matthew Reiter <matthewreiter@hotmail.com>
2026-04-11 20:54:26 -0400

    Update the font generator for Factorio 2.0.

--- Blueprint Generator/FontGenerator.cs
+++ Blueprint Generator/FontGenerator.cs
@@ -1,199 +1,190 @@
 ﻿using BlueprintCommon;
 using BlueprintCommon.Constants;
 using BlueprintCommon.Models;
+using BlueprintGenerator.Models;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
-using static BlueprintGenerator.ConnectionUtil;
+using System.Runtime.Versioning;
 using Icon = BlueprintCommon.Models.Icon;
 
-namespace BlueprintGenerator
+namespace BlueprintGenerator;
+
+[SupportedOSPlatform("windows")]
+public class FontGenerator : IBlueprintGenerator
 {
-    public class FontGenerator : IBlueprintGenerator
+    public Blueprint Generate(IConfigurationRoot configuration)
     {
-        public Blueprint Generate(IConfigurationRoot configuration)
+        return Generate(configuration.Get<FontConfiguration>());
+    }
+
+    public static Blueprint Generate(FontConfiguration configuration)
+    {
+        var fontImageFile = configuration.FontImage;
+        var combinatorsPerRow = configuration.CombinatorsPerRow ?? 5;
+        var useOneSignalPerRow = configuration.UseOneSignalPerRow ?? false;
+        var inputSignal = configuration.InputSignal ?? VirtualSignalNames.Dot;
+        var widthSignal = configuration.WidthSignal;
+        var heightSignal = configuration.HeightSignal;
+        var signals = configuration.Signals.Contains(',') ? [.. configuration.Signals.Split(',')] : configuration.Signals.Select(VirtualSignalNames.LetterOrDigit).ToList();
+
+        var font = FontUtil.ReadFont(fontImageFile);
+        var characters = font.Characters;
+
+        List<Entity> entities = [];
+        List<Wire> wires = [];
+        List<Entity> matchers = [];
+
+        if (heightSignal is not null)
         {
-            return Generate(configuration.Get<FontConfiguration>());
+            characters.Add(new FontUtil.Character
+            {
+                CharacterCode = '\n',
+                GlyphPixels = new bool[font.Height, 0]
+            });
         }
 
-        public static Blueprint Generate(FontConfiguration configuration)
+        for (var characterIndex = 0; characterIndex < characters.Count; characterIndex++)
         {
-            var fontImageFile = configuration.FontImage;
-            var combinatorsPerRow = configuration.CombinatorsPerRow ?? 5;
-            var useOneSignalPerRow = configuration.UseOneSignalPerRow ?? false;
-            var inputSignal = configuration.InputSignal ?? VirtualSignalNames.Dot;
-            var widthSignal = configuration.WidthSignal;
-            var heightSignal = configuration.HeightSignal;
-            var signals = configuration.Signals.Contains(',') ? configuration.Signals.Split(',').ToList() : configuration.Signals.Select(signal => VirtualSignalNames.LetterOrDigit(signal)).ToList();
+            var character = characters[characterIndex];
+            var glyphPixels = character.GlyphPixels;
+            var height = glyphPixels.GetLength(0);
+            var width = glyphPixels.GetLength(1);
 
-            const int maxFilters = 20;
+            var glyphFilters = new List<Filter>();
 
-            var font = FontUtil.ReadFont(fontImageFile);
-            var characters = font.Characters;
-
-            var entities = new List<Entity>();
-            var characterEntities = new List<(Entity Matcher, List<Entity> Glyph)>();
+            if (widthSignal != null)
+            {
+                glyphFilters.Add(Filter.Create(widthSignal, width));
+            }
 
             if (heightSignal != null)
             {
-                characters.Add(new FontUtil.Character
-                {
-                    CharacterCode = '\n',
-                    GlyphPixels = new bool[font.Height, 0]
-                });
+                glyphFilters.Add(Filter.Create(heightSignal, height));
             }
 
-            var combinatorX = 0;
-
-            for (var characterIndex = 0; characterIndex < characters.Count; characterIndex++)
+            for (int y = 0; y < height; y++)
             {
-                var character = characters[characterIndex];
-                var glyphPixels = character.GlyphPixels;
-                var height = glyphPixels.GetLength(0);
-                var width = glyphPixels.GetLength(1);
-
-                if (characterIndex % combinatorsPerRow == 0)
+                if (useOneSignalPerRow)
                 {
-                    combinatorX = 0;
-                }
+                    var rowSignal = 0;
 
-                var glyphFilters = new List<Filter>();
-
-                if (widthSignal != null)
-                {
-                    glyphFilters.Add(Filter.Create(widthSignal, width));
-                }
-
-                if (heightSignal != null)
-                {
-                    glyphFilters.Add(Filter.Create(heightSignal, height));
-                }
-
-                for (int y = 0; y < height; y++)
-                {
-                    if (useOneSignalPerRow)
+                    for (int x = 0; x < width; x++)
                     {
-                        var rowSignal = 0;
-
-                        for (int x = 0; x < width; x++)
+                        if (glyphPixels[y, x])
                         {
-                            if (glyphPixels[y, x])
+                            rowSignal |= 1 << x;
+                        }
+                    }
+
+                    glyphFilters.Add(Filter.Create(signals[y], rowSignal));
+                }
+                else
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (glyphPixels[y, x])
+                        {
+                            glyphFilters.Add(Filter.Create(signals[y * width + x]));
+                        }
+                    }
+                }
+            }
+
+            var combinatorX = (characterIndex % combinatorsPerRow - combinatorsPerRow) * 3;
+            var combinatorY = characterIndex / combinatorsPerRow;
+
+            var matcher = new Entity
+            {
+                Name = ItemNames.DeciderCombinator,
+                Position = new Position
+                {
+                    X = combinatorX + 0.5,
+                    Y = combinatorY
+                },
+                Direction = Direction.Left,
+                Control_behavior = new ControlBehavior
+                {
+                    Decider_conditions = new DeciderConditions
+                    {
+                        Conditions =
+                        [
+                            new()
                             {
-                                rowSignal |= 1 << x;
+                                First_signal = SignalID.Create(inputSignal),
+                                First_signal_networks = new() { Red = true },
+                                Constant = character.CharacterCode,
+                                Comparator = Comparators.IsEqual
                             }
-                        }
-
-                        glyphFilters.Add(Filter.Create(signals[y], rowSignal));
-                    }
-                    else
-                    {
-                        for (int x = 0; x < width; x++)
-                        {
-                            if (glyphPixels[y, x])
+                        ],
+                        Outputs =
+                        [
+                            new()
                             {
-                                glyphFilters.Add(Filter.Create(signals[y * width + x]));
+                                Signal = SignalID.Create(VirtualSignalNames.Everything),
+                                Copy_count_from_input = true,
+                                Networks = new() { Green = true }
                             }
-                        }
+                        ]
                     }
                 }
-
-                var combinatorY = characterIndex / combinatorsPerRow;
-
-                var matcher = new Entity
-                {
-                    Name = ItemNames.DeciderCombinator,
-                    Position = new Position
-                    {
-                        X = (characterIndex % combinatorsPerRow - combinatorsPerRow) * 2 + 0.5,
-                        Y = combinatorY
-                    },
-                    Direction = Direction.Left,
-                    Control_behavior = new ControlBehavior
-                    {
-                        Decider_conditions = new DeciderConditions
-                        {
-                            First_signal = SignalID.Create(inputSignal),
-                            Constant = character.CharacterCode,
-                            Comparator = Comparators.IsEqual,
-                            Output_signal = SignalID.Create(VirtualSignalNames.Everything),
-                            Copy_count_from_input = true
-                        }
-                    }
-                };
-                entities.Add(matcher);
-
-                var glyph = new List<Entity>();
-
-                for (int index = 0; index < (glyphFilters.Count + maxFilters - 1) / maxFilters; index++)
-                {
-                    var glyphPart = new Entity
-                    {
-                        Name = ItemNames.ConstantCombinator,
-                        Position = new Position
-                        {
-                            X = combinatorX++,
-                            Y = combinatorY
-                        },
-                        Direction = Direction.Down,
-                        Control_behavior = new ControlBehavior
-                        {
-                            Filters = glyphFilters.Skip(index * maxFilters).Take(maxFilters).ToList()
-                        }
-                    };
-                    glyph.Add(glyphPart);
-                    entities.Add(glyphPart);
-                }
-
-                characterEntities.Add((matcher, glyph));
-            }
-
-            BlueprintUtil.PopulateEntityNumbers(entities);
-
-            for (int characterIndex = 0; characterIndex < characterEntities.Count; characterIndex++)
-            {
-                var (matcher, glyph) = characterEntities[characterIndex];
-
-                AddConnection(CircuitColor.Green, matcher, CircuitId.Input, glyph[0], null); // Connect to constant combinators holding glyphs
-
-                var adjacentCharacterIndex = characterIndex - (characterIndex / combinatorsPerRow == 0 ? 1 : combinatorsPerRow);
-                if (adjacentCharacterIndex >= 0)
-                {
-                    var adjacentMatcher = characterEntities[adjacentCharacterIndex].Matcher;
-
-                    AddConnection(CircuitColor.Red, matcher, CircuitId.Input, adjacentMatcher, CircuitId.Input); // Connect inputs together
-                    AddConnection(CircuitColor.Green, matcher, CircuitId.Output, adjacentMatcher, CircuitId.Output); // Connect outputs together
-                }
-
-                // Connections between glyph parts
-                for (int index = 1; index < glyph.Count; index++)
-                {
-                    AddConnection(CircuitColor.Green, glyph[index], null, glyph[index - 1], null);
-                }
-            }
-
-            return new Blueprint
-            {
-                Label = $"{font.Width}x{font.Height} Font",
-                Icons =
-                [
-                    Icon.Create(ItemNames.ConstantCombinator),
-                    Icon.Create(VirtualSignalNames.LetterOrDigit('A')),
-                    Icon.Create(VirtualSignalNames.LetterOrDigit('B')),
-                    Icon.Create(VirtualSignalNames.LetterOrDigit('C'))
-                ],
-                Entities = entities
             };
-        }
-    }
+            entities.Add(matcher);
 
-    public class FontConfiguration
-    {
-        public string FontImage { get; init; }
-        public int? CombinatorsPerRow { get; init; }
-        public bool? UseOneSignalPerRow { get; init; }
-        public string InputSignal { get; init; }
-        public string WidthSignal { get; init; }
-        public string HeightSignal { get; init; }
-        public string Signals { get; init; }
+            var glyph = new Entity
+            {
+                Name = ItemNames.ConstantCombinator,
+                Position = new Position
+                {
+                    X = combinatorX + 2,
+                    Y = combinatorY
+                },
+                Direction = Direction.Left,
+                Control_behavior = new ControlBehavior
+                {
+                    Sections = Sections.Create(glyphFilters)
+                }
+            };
+            entities.Add(glyph);
+            matchers.Add(matcher);
+
+            wires.Add(new((matcher, ConnectionType.Green1), (glyph, ConnectionType.Green1))); // Connect to constant combinators holding glyphs
+
+            var adjacentCharacterIndex = characterIndex - (characterIndex / combinatorsPerRow == 0 ? 1 : combinatorsPerRow);
+            if (adjacentCharacterIndex >= 0)
+            {
+                var adjacentMatcher = matchers[adjacentCharacterIndex];
+
+                wires.Add(new((matcher, ConnectionType.Red1), (adjacentMatcher, ConnectionType.Red1))); // Connect inputs together
+                wires.Add(new((matcher, ConnectionType.Green2), (adjacentMatcher, ConnectionType.Green2))); // Connect outputs together
+            }
+        }
+
+        BlueprintUtil.PopulateEntityNumbers(entities);
+
+        return new Blueprint
+        {
+            Label = $"{font.Width}x{font.Height} Font",
+            Icons =
+            [
+                Icon.Create(ItemNames.ConstantCombinator),
+                Icon.Create(VirtualSignalNames.LetterOrDigit('A')),
+                Icon.Create(VirtualSignalNames.LetterOrDigit('B')),
+                Icon.Create(VirtualSignalNames.LetterOrDigit('C'))
+            ],
+            Entities = entities,
+            Wires = wires.ToArrayList()
+        };
     }
+}
+
+public class FontConfiguration
+{
+    public string FontImage { get; init; }
+    public int? CombinatorsPerRow { get; init; }
+    public bool? UseOneSignalPerRow { get; init; }
+    public string InputSignal { get; init; }
+    public string WidthSignal { get; init; }
+    public string HeightSignal { get; init; }
+    public string Signals { get; init; }
 }
--- Blueprint Generator/FontUtil.cs
+++ Blueprint Generator/FontUtil.cs
@@ -1,112 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.Versioning;
 
-namespace BlueprintGenerator
+namespace BlueprintGenerator;
+
+public static class FontUtil
 {
-    public static class FontUtil
+    [SupportedOSPlatform("windows")]
+    public static Font ReadFont(string fontImageFile)
     {
-        public static Font ReadFont(string fontImageFile)
+        using var fontImage = new Bitmap(fontImageFile);
+
+        static bool IsRed(Color color) => color.R > 192 && color.G < 64 && color.B < 64;
+
+        var height = 0;
+        for (int y = 0; y + 1 < fontImage.Height; y++)
         {
-            using var fontImage = new Bitmap(fontImageFile);
-
-            static bool IsRed(Color color) => color.R > 192 && color.G < 64 && color.B < 64;
-
-            var height = 0;
-            for (int y = 0; y + 1 < fontImage.Height; y++)
+            if (IsRed(fontImage.GetPixel(1, y + 1)))
             {
-                if (IsRed(fontImage.GetPixel(1, y + 1)))
-                {
-                    height = y;
-                    break;
-                }
+                height = y;
+                break;
             }
+        }
 
-            if (height == 0)
+        if (height == 0)
+        {
+            throw new Exception("Unable to determine glyph height");
+        }
+
+        var fullHeight = height + 2;
+        var fullWidth = 0;
+
+        var characters = new List<Character>();
+        var currentCharacterCode = 0;
+        var maxWidth = 0;
+
+        for (var glyphY = 0; glyphY < fontImage.Height - 1; glyphY += fullHeight)
+        {
+            for (var glyphX = 0; glyphX < fontImage.Width - 1; glyphX += fullWidth)
             {
-                throw new Exception("Unable to determine glyph height");
-            }
-
-            var fullHeight = height + 2;
-            var fullWidth = 0;
-
-            var characters = new List<Character>();
-            var currentCharacterCode = 0;
-            var maxWidth = 0;
-
-            for (var glyphY = 0; glyphY < fontImage.Height - 1; glyphY += fullHeight)
-            {
-                for (var glyphX = 0; glyphX < fontImage.Width - 1; glyphX += fullWidth)
+                var width = 0;
+                for (var x = 0; glyphX + x + 1 < fontImage.Width; x++)
                 {
-                    var width = 0;
-                    for (var x = 0; glyphX + x + 1 < fontImage.Width; x++)
+                    if (IsRed(fontImage.GetPixel(glyphX + x + 1, glyphY + 1)))
                     {
-                        if (IsRed(fontImage.GetPixel(glyphX + x + 1, glyphY + 1)))
-                        {
-                            width = x;
-                            break;
-                        }
-                    }
-
-                    if (width == 0)
-                    {
+                        width = x;
                         break;
                     }
-
-                    fullWidth = width + 1;
-
-                    if (width > maxWidth)
-                    {
-                        maxWidth = width;
-                    }
-
-                    var baseCharacterIndicator = fontImage.GetPixel(glyphX, glyphY);
-                    if (baseCharacterIndicator.R == 0)
-                    {
-                        currentCharacterCode = baseCharacterIndicator.G;
-                    }
-                    else
-                    {
-                        currentCharacterCode++;
-                    }
-
-                    var glyphPixels = new bool[height, width];
-                    for (var y = 0; y < height; y++)
-                    {
-                        for (int x = 0; x < width; x++)
-                        {
-                            var pixel = fontImage.GetPixel(glyphX + x + 1, glyphY + y + 1);
-                            glyphPixels[y, x] = pixel.ToArgb() == Color.Black.ToArgb();
-                        }
-                    }
-
-                    characters.Add(new Character
-                    {
-                        CharacterCode = currentCharacterCode,
-                        GlyphPixels = glyphPixels
-                    });
                 }
+
+                if (width == 0)
+                {
+                    break;
+                }
+
+                fullWidth = width + 1;
+
+                if (width > maxWidth)
+                {
+                    maxWidth = width;
+                }
+
+                var baseCharacterIndicator = fontImage.GetPixel(glyphX, glyphY);
+                if (baseCharacterIndicator.R == 0)
+                {
+                    currentCharacterCode = baseCharacterIndicator.G;
+                }
+                else
+                {
+                    currentCharacterCode++;
+                }
+
+                var glyphPixels = new bool[height, width];
+                for (var y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        var pixel = fontImage.GetPixel(glyphX + x + 1, glyphY + y + 1);
+                        glyphPixels[y, x] = pixel.ToArgb() == Color.Black.ToArgb();
+                    }
+                }
+
+                characters.Add(new Character
+                {
+                    CharacterCode = currentCharacterCode,
+                    GlyphPixels = glyphPixels
+                });
             }
-
-            return new Font
-            {
-                Width = maxWidth,
-                Height = height,
-                Characters = characters
-            };
         }
 
-        public class Font
+        return new Font
         {
-            public int Width { get; init; }
-            public int Height { get; init; }
-            public List<Character> Characters { get; init; }
-        }
+            Width = maxWidth,
+            Height = height,
+            Characters = characters
+        };
+    }
 
-        public class Character
-        {
-            public int CharacterCode { get; init; }
-            public bool[,] GlyphPixels { get; init; }
-        }
+    public class Font
+    {
+        public int Width { get; init; }
+        public int Height { get; init; }
+        public List<Character> Characters { get; init; }
+    }
+
+    public class Character
+    {
+        public int CharacterCode { get; init; }
+        public bool[,] GlyphPixels { get; init; }
     }
 }
